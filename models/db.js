65619c6e0f5768e1974bf73d5f2cd4b7398e9bc7
xiaoconghu <huxc10086@163.com>
2018-04-14 01:00:49 +0800

    mongodb

---
+++ models/db.js
@@ -0,0 +1,7 @@
+var db = require('mongoskin').db('localhost:27017');
+
+// db.collection('xiao').find().toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+// });
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../models/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  db.collection('xiao').find().toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+  });
   res.render('index', { title: 'Express' });
 });
 
