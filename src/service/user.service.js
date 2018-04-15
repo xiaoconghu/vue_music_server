70be7196b77a3af99cf47c1983f5dd5ec83ed964
xiaoconghu <huxc10086@163.com>
2018-04-15 22:43:46 +0800

    优化

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -26,7 +26,6 @@ class UserService {
                 reject(failed)
             })
         })
-
     }
 }
 
--- src/utils/common-utils.js
+++ src/utils/common-utils.js
@@ -2,9 +2,6 @@
  * Created by xiaoconghu on 2018/4/14.
  */
 class Result {
-    code;
-    data;
-    msg;
     constructor(code,data,msg) {
         this.code = code;
         this.data = data;
